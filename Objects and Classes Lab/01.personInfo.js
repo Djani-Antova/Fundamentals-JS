80c375e19bf5545debf11879b7708ed4598a447a
djaniAntova <djaniantova@gmail.com>
2022-10-24 13:19:34 +0300

    Create 01.personInfo.js
    Add tasks for Lab
    Problem solutions for Lab: Objects and Classes

---
+++ Objects and Classes Lab/01.personInfo.js
@@ -0,0 +1,14 @@
+function personInfo(firstName,lastName, age ) {
+    let res = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+    };
+    
+    return res;
+    
+}
+console.log(personInfo("Peter", 
+"Pan",
+"20"
+))
