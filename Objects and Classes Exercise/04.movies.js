8a9d796d51e94ae74b993779487ea67827c1eeb7
djaniAntova <djaniantova@gmail.com>
2022-10-29 00:40:30 +0300

    Create 04.movies.js
    Add task solution to Exercise: Objects and Classes

---
+++ Objects and Classes Exercise/04.movies.js
@@ -0,0 +1,46 @@
+function objectDemo(arr) {
+
+    let movies = [];
+
+    arr.forEach(el => {
+
+        if (el.includes('addMovie ')) {
+            let movie = el.replace('addMovie ', '');
+            movies.push({name: movie });
+
+        }
+        else if (el.includes('directedBy')) {
+            let movieInfo = el.split(' directedBy ');
+            let name = movieInfo[0];
+            let director = movieInfo[1]; // or let[name, director] = el.split(' directedBy ');
+            movies.forEach(movie => {
+                if (movie.name === name) {
+                    movie.director = director;
+                }
+            }) 
+
+        } else if (el.includes('onDate')) {
+            let [name,date] = el.split(' onDate ');
+            movies.forEach(movie => {
+                if (movie.name === name) {
+                    movie.date = date;
+                }
+            }) 
+        }    
+    });
+       movies.forEach(movie => {
+        if(movie.name && movie.director && movie.date){
+            console.log(JSON.stringify(movie));
+        }
+    })
+
+}
+objectDemo([
+    'addMovie Fast and Furious',
+    'addMovie Godfather',
+    'Inception directedBy Christopher Nolan',
+    'Godfather directedBy Francis Ford Coppola',
+    'Godfather onDate 29.07.2018',
+    'Fast and Furious onDate 30.07.2018',
+    'Batman onDate 01.08.2018',
+    'Fast and Furious directedBy Rob Cohen'])
