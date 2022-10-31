6f65137bd1cfb9183d6625597bb068dbb489f88c
djaniAntova <djaniantova@gmail.com>
2022-10-31 19:19:22 +0200

    Create 07.classVehicle.js
    Add solution for Exercise: Objects and Classes

---
+++ Objects and Classes Exercise/07.classVehicle.js
@@ -0,0 +1,22 @@
+
+class Vehicle  {
+
+    constructor (type, model, parts, fuel) {
+        this.type = type;
+        this.model = model;
+        this.parts = parts;
+        this.fuel = fuel;
+        this.parts.quality = parts.engine * parts.power 
+
+    }
+    drive(fuelLoss) {
+        this.fuel -= fuelLoss;
+    }
+}
+let parts = { engine: 6, power: 100 };
+let vehicle = new Vehicle('a', 'b', parts, 200);
+vehicle.drive(100);
+console.log(vehicle.fuel);
+console.log(vehicle.parts.quality);
+
+
