ac4be184abfa76f0ae287b919a90eb29ce1f7b97
djaniAntova <djaniantova@gmail.com>
2022-11-12 11:08:42 +0200

    Create 01.revealWords2.js
    Add better task solution for Exercise: Text Processing

---
+++ Text Processing Exercise/01.revealWords2.js
@@ -0,0 +1,13 @@
+function revealWords(words, text) {
+
+    let wordsArray = words.split(", ");
+    wordsArray.forEach(word => {
+        const match = `${'*'.repeat(word.length)}`;
+        text = text.replace(match, word)
+    });
+    
+    console.log(text);    
+}
+
+revealWords('great, learning',
+'softuni is ***** place for ******** new programming languages')
